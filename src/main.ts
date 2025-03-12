7283c162bd7abca50a766327c22689f21e62386e
Devconet <42451250+devconet@users.noreply.github.com>
2025-03-12 22:47:11 +0100

    Added solana wallet connection code into main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,25 @@ import "./assets/css/nucleo-icons.css";
 import "./assets/css/nucleo-svg.css";
 import MaterialDashboard from "./material-dashboard";
 
+import SolanaWallets from "solana-wallets-vue";
+import "solana-wallets-vue/styles.css";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
+const walletOptions = {
+    wallets: [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
+    ],
+    autoConnect: true,
+  };
+
 const appInstance = createApp(App);
 appInstance.use(store);
 appInstance.use(router);
 appInstance.use(MaterialDashboard);
+appInstance.use(SolanaWallets, walletOptions);
 appInstance.mount("#app");
