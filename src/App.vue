c6afdef3ca28e47bbb4c0ed026b73606e33263cc
Devconet <42451250+devconet@users.noreply.github.com>
2025-03-20 22:35:19 +0100

    Importing Dashboard App.vue and main.ts files

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,82 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<!--
+=========================================================
+* Vue Material Dashboard 2 - v3.1.0
+=========================================================
 
+* Product Page: https://creative-tim.com/product/vue-material-dashboard-2
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <sidenav
+    :custom_class="color"
+    :class="[isRTL ? 'fixed-end' : 'fixed-start']"
+    v-if="showSidenav"
+  />
+  <main
+    class="main-content position-relative max-height-vh-100 h-100 overflow-x-hidden"
+  >
+    <!-- nav -->
+    <navbar
+      :class="[isNavFixed ? navbarFixed : '', isAbsolute ? absolute : '']"
+      :color="isAbsolute ? 'text-white opacity-8' : ''"
+      :minNav="navbarMinimize"
+      v-if="showNavbar"
+    />
+    <router-view />
+    <app-footer v-show="showFooter" />
+    <configurator
+      :toggle="toggleConfigurator"
+      :class="[showConfig ? 'show' : '', hideConfigButton ? 'd-none' : '']"
+    />
+  </main>
 </template>
+<script>
+import Sidenav from "./examples/Sidenav/index.vue";
+import Configurator from "@/examples/Configurator.vue";
+import Navbar from "@/examples/Navbars/Navbar.vue";
+import AppFooter from "@/examples/Footer.vue";
+import { mapMutations, mapState } from "vuex";
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+export default {
+  name: "App",
+  components: {
+    Sidenav,
+    Configurator,
+    Navbar,
+    AppFooter,
+  },
+  methods: {
+    ...mapMutations(["toggleConfigurator", "navbarMinimize"]),
+  },
+  computed: {
+    ...mapState([
+      "isRTL",
+      "color",
+      "isAbsolute",
+      "isNavFixed",
+      "navbarFixed",
+      "absolute",
+      "showSidenav",
+      "showNavbar",
+      "showFooter",
+      "showConfig",
+      "hideConfigButton",
+    ]),
+  },
+  beforeMount() {
+    this.$store.state.isTransparent = "bg-transparent";
+
+    const sidenav = document.getElementsByClassName("g-sidenav-show")[0];
+
+    if (window.innerWidth > 1200) {
+      sidenav.classList.add("g-sidenav-pinned");
+    }
+  },
+};
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+//import router from "./router";
+import "./assets/css/nucleo-icons.css";
+import "./assets/css/nucleo-svg.css";
+import MaterialDashboard from "./material-dashboard.ts";
 
-createApp(App).mount('#app')
+//import SolanaWallets from "solana-wallets-vue";
+//import "solana-wallets-vue/styles.css";
+//import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+//import {
+//  PhantomWalletAdapter,
+//  SolflareWalletAdapter,
+//} from "@solana/wallet-adapter-wallets";
+
+/*const walletOptions = {
+    wallets: [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
+    ],
+    autoConnect: true,
+  };*/
+
+const appInstance = createApp(App);
+//appInstance.use(store);
+//appInstance.use(router);
+appInstance.use(MaterialDashboard);
+//appInstance.use(SolanaWallets, walletOptions);
+appInstance.mount("#app");
